fbe343a91fdcc81596975de2ba62722cf2f3218d
Akram Ammis <122386669+AkramAmmis@users.noreply.github.com>
2026-01-22 09:37:54 +0100

    Routes Links to navi added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {EmployeeListComponent} from "./employee-list/employee-list.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: EmployeeListComponent },
+  { path: 'employee', component: EmployeeListComponent },
+  { path: 'qualification', component: EmployeeListComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+];
+
