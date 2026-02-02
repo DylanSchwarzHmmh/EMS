716bc1e94593a63d8fc29f00d853d89a39f89184
Akram Ammis <akramammis0@gmail.com>
2026-02-02 16:32:39 +0100

    connected the qualification list dynamically to the service using Angular, and integrated the delete functionality.

--- src/app/qualification/qualification.component.ts
+++ src/app/qualification/qualification.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {QualificationService} from "../services/quali/qualification.service";
+import {Qualification} from "../services/quali/qualification.model";
 
 @Component({
   selector: 'app-qualification',
@@ -8,9 +9,62 @@ import {QualificationService} from "../services/quali/qualification.service";
   templateUrl: './qualification.component.html',
   styleUrl: './qualification.component.css'
 })
-export class QualificationComponent {
-  constructor(private qualiService: QualificationService) {
-    // Eine Zeile, um den Motor zu starten
-    this.qualiService.getQualifications().subscribe();
+export class QualificationComponent implements OnInit {
+
+  private qualiServ = inject(QualificationService);
+  qualifications: Qualification[] = [];
+
+  ngOnInit(): void {
+    this.loadQualifications();
+
+
   }
+
+  loadQualifications(): void {
+    this.qualiServ.getQualifications().subscribe({
+      next: data => {
+        this.qualifications = data;
+      }
+    })
+    console.log("Load Qualifications"+ this.qualifications);
+  }
+
+  protected onDelete(id: number) {
+    this.qualiServ.deleteQualifications(id).subscribe({
+      next: () => {
+        // 2. ERFOLG! Jetzt rufen wir unsere Lade-Funktion von vorhin auf
+        // Damit wird die Liste vom Server neu geholt und das HTML aktualisiert sich automatisch.
+        this.loadQualifications();
+        console.log('Qualifikation mit ID ' + id + ' wurde gelöscht.');}
+      })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //constructor(private qualiService: QualificationService) {
+    // Eine Zeile, um den Motor zu starten
+    //this.qualiService.deleteQualifications(17).subscribe()
+    //this.qualiService.putQualifications(18,"Python").subscribe()
+    //this.qualiService.postQualifications("aa").subscribe()
+    //this.qualiService.getQualifications().subscribe();}
+
+  //Den Service injecten (deinen Koch rufen).
+    //Eine Variable erstellen, in der die Liste gespeichert wird.
+
+  //Den Service aufrufen, sobald die Komponente "geboren" wird (ngOnInit).
+
 }
