97f7ee07307cab0d3119a966127c238646cb8bcd
Akram Ammis <akramammis0@gmail.com>
2026-02-03 18:45:18 +0100

    add validation and toast notifications for qualifications

--- src/app/qualification/qualification.component.ts
+++ src/app/qualification/qualification.component.ts
@@ -1,23 +1,39 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {QualificationService} from "../services/quali/qualification.service";
 import {Qualification} from "../services/quali/qualification.model";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-qualification',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './qualification.component.html',
   styleUrl: './qualification.component.css'
 })
 export class QualificationComponent implements OnInit {
 
   isAdding = false;
+  showToast= false;
+  toastMessage= "";
+  toastClass= ""; //textColor
 
   showAddRow(){
     this.isAdding = true;
   }
   hideAddRow(){
     this.isAdding = false;
+  }
+
+  triggerNotification(nachricht: string, istFehler: boolean) {
+    this.toastMessage = nachricht; // 1. Erst den Text setzen
+    this.toastClass = istFehler ? 'text-danger' : 'text-success';
+    this.showToast = true;
+    setTimeout(() => {
+      this.showToast = false ;
+    }, 2500);//
+
   }
 
   private qualiServ = inject(QualificationService);
@@ -42,7 +58,7 @@ export class QualificationComponent implements OnInit {
         // 2. ERFOLG! Jetzt rufen wir unsere Lade-Funktion von vorhin auf
         // Damit wird die Liste vom Server neu geholt und das HTML aktualisiert sich automatisch.
         this.loadQualifications();
-        console.log('Qualifikation mit ID ' + id + ' wurde gelöscht.');}
+        this.triggerNotification("Qualifikation wurde gelöscht", false)}
       })
   }
 
@@ -74,13 +90,25 @@ export class QualificationComponent implements OnInit {
 
   //Den Service aufrufen, sobald die Komponente "geboren" wird (ngOnInit).
 
+
   protected onSave(skillName: string) {
+    console.log(this.qualifications)
+    if (!skillName || skillName.trim() === '') {
+      this.triggerNotification('Bitte gib einen Namen ein!', true); // true = rot
+      return;
+    }
+    const exists = this.qualifications.some(q =>q.skill === skillName);
+    if (exists) {
+      this.triggerNotification("Diese Qualifikation existiert bereits!", true);
+    }
+
     this.qualiServ.postQualifications(skillName).subscribe({
       next: () => {
         // 2. ERFOLG! Jetzt rufen wir unsere Lade-Funktion von vorhin auf
         // Damit wird die Liste vom Server neu geholt und das HTML aktualisiert sich automatisch.
         this.loadQualifications();
         this.hideAddRow()
+        this.triggerNotification('Qualifikation wurde hizugefügt', false);
    }
     })
   }
