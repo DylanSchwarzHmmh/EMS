b5872697620c2541294a3a5c6b31673bde8e21f1
Akram Ammis <122386669+AkramAmmis@users.noreply.github.com>
2026-02-04 19:36:21 +0100

    erorr fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import {EmployeeListComponent} from "./employee-list/employee-list.component";
 import {QualificationComponent} from "./qualification/qualification.component";
 import { HomeComponent } from './home/home.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { CallbackComponent } from './callback/callback.component';
 import { authGuard } from './auth.guard';
 
@@ -10,7 +9,7 @@ export const routes: Routes = [
   { path: 'dashboard', component: EmployeeListComponent },
   { path: 'employee', component: EmployeeListComponent },
   { path: 'qualification', component: QualificationComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '', component: HomeComponent },
   { path: 'callback', component: CallbackComponent },
   { path: 'employees', component: EmployeeListComponent, canActivate: [authGuard] },
